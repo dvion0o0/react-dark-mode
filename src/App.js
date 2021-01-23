9a7eba40d714685d49c41ea6d3cb8eb9fd227a89
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-23 23:15:57 +0530

    darkmode

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
+function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggle = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggle}>
+            toggle
+          </button>
+        </div>
+        <section className="articles">
+          {data.map((item) => {
+            return <Article key={item.id} {...item} />;
+          })}
+        </section>
+      </nav>
+    </main>
+  );
+}
+
+export default App;
